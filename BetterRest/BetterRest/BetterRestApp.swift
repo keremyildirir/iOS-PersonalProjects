54ddda25e1b2fa24a32274c3de9d7663b20c9b20
cci-kyildirir <150420107+cci-kyildirir@users.noreply.github.com>
2024-08-21 17:13:59 +0300

    Project 4 (BetterRest), part one
    A project that calculates necessary sleep time using CoreML

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kerem Yildirir on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
