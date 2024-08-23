8cb3392bd5d154d0a73f689157be523d19f323c7
cci-kyildirir <150420107+cci-kyildirir@users.noreply.github.com>
2024-08-23 14:30:56 +0300

    Project 5 (WordScramble), part one

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kerem Yildirir on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
