410ea6ec70e06d11d60a00d46ed56569eadccc8c
Kerem Yildirir <kerem.yildirir@cci.com.tr>
2024-09-05 13:49:29 +0300

    Project 8 (Moonshot), part one
    Implement Moonshot app which lets users learn about the missions and astronauts that formed NASA’s Apollo space program.

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Kerem Yildirir on 3.09.2024.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kerem Yildirir on 3.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
