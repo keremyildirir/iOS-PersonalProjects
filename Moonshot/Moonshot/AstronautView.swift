5d5f1030141f0f059a956392f895b4a21e080c6c
Kerem Yildirir <kerem.yildirir@cci.com.tr>
2024-09-06 11:40:41 +0300

    Project 8 (Moonshot), part two
    Implement detail views

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,37 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Kerem Yildirir on 6.09.2024.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(20)
+
+                Text(astronaut.description)
+                    .padding()
+            }
+            .padding()
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
