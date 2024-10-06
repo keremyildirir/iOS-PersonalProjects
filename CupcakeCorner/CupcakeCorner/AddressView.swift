4a0acdab4e42c38ff7fc0863f6f184fabdf56924
Kerem Yildirir <kerem.yildirir@cci.com.tr>
2024-10-06 18:31:07 +0300

    Project 10 (CupcakeCorner), part one

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Kerem Yildirir on 6.10.2024.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
